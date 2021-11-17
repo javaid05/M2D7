a4893a7705132179ea76e69b66da5411d07ec70c
javaid05 <javaidmalik.khan@gmail.com>
2021-11-17 13:14:09 +0000

    update

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
-// const newLink = function () {
-//   let navbar = document.querySelector(".nav");
-//   let navlink = document.createElement("a");
-//   navlink.classList = "p-2 text-muted";
-//   navlink.href = "#";
-//   navlink.innerText = "New Link";
-//   navbar.appendChild(navlink);
-// };
-// newLink();
+const newLink = function () {
+  console.log("ok");
+  let navbar = document.querySelector(".nav");
+  let navlink = document.createElement("a");
+  navlink.className = "p-2 text-muted";
+  navlink.href = "#";
+  navlink.innerText = "New Link";
+  navbar.appendChild(navlink);
+};
+newLink();
 // EX12) Write a function to change the color of the main title
 
 // const changeMainTItle = function () {
@@ -18,9 +19,10 @@
 // };
 // changeMainTItle();
 // EX13) Write a function to change the background of the jumbotron
-const changeJumbotronBg = function () {
-  const jumbotron = document.querySelector(".jumbotron");
-  jumbotron.classList.remove("bg-dark");
-  jumbotron.classList.add("bg-primary");
-};
-changeJumbotronBg();
+// const changeJumbotronBg = function () {
+//   const jumbotron = document.querySelector(".jumbotron");
+//   jumbotron.classList.remove("bg-dark");
+//   jumbotron.classList.add("bg-primary");
+// };
+// changeJumbotronBg();
+//EX15) Write a function to change the column size for heading in jumbotron
